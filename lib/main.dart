2dc8623d0c634541ac77d8618a4eebefade15c85
rian <rianpr20@gmail.com>
2021-08-27 18:00:03 +0700

    create new rute, and show favorite

--- lib/main.dart
+++ lib/main.dart
@@ -60,6 +60,7 @@ class _RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Startup Name Generator'),
+        actions: [IconButton(onPressed: pushSaved, icon: Icon(Icons.list))],
       ),
       body: buildSuggestions(),
     );
@@ -98,6 +99,36 @@ class _RandomWordsState extends State<RandomWords> {
           }
         });
       },
+    );
+  }
+
+  void pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final tiles = saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
     );
   }
 }
